a090d9754d21473c9f7fbc706fd69310927dbfd1
Matthew-Middleton <36935493+Matthew-Middleton@users.noreply.github.com>
2019-11-06 22:31:01 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <msp430.h> 
 
+void configTimerA(unsigned int delayCycles);
 
-/**http://e2e.ti.com/support/microcontrollers/msp430/f/166/t/304138
+/**
  * main.c
  */
 int main(void)
@@ -17,15 +18,35 @@ int main(void)
     PM5CTL0 &= ~LOCKLPM5;
 
     CSCTL0_H = CSKEY_H;     /*Unlocks the CS registers for manipulation*/
-    CSCTL1 = DCOFSEL_0;     /*Sets the DCO to 1 MHz*/
-    CSCTL2 = SELM__DCOCLK;  /*Selects MCLK as the desired clock*/
-    CSCTL3 = DIVM_0;        /*Sets the divider for MCLK to 1*/
+    CSCTL1 = DCOFSEL_0;     /*Sets the DCO to 1 MHz: Default frequency*/
+    CSCTL2 = SELM__DCOCLK | SELS__DCOCLK;  /*Selects MCLK and SMCLK as the desired clock*/
+    CSCTL3 = DIVM__1 | DIVS__1;        /*Sets the divider for MCLK and SMCLK to 1*/
 
     /*Delay by ~10us to let DCO settle. 30 cycles = 20 cycles buffer + (10us / (1/1MHz))*/
     __delay_cycles(30);
-
     CSCTL0_H = 0;           /*Lock CS registers*/
 
-
+    configTimerA(50000);
+    while(1)
+    {
+        _bis_SR_register(LPM3_bits+GIE);//what?
+    }
 	return 0;
+}
+
+//should configure to check for unsigned overflow
+void configTimerA(unsigned int delayCycles)
+{
+
+    TA0CCTL0 |= CCIE;       /*Enable Interrupts on Timer*/
+    TA0CCR0 = delayCycles;  /*Number of cycles in the timer*/
+    TA0CTL |= TASSEL__SMCLK;/*Use SMCLK as source for timer*/
+    TA0CTL |= MC_1;         /*Use UP mode timer*/
+}
+
+#pragma vector = TIMER0_A0_VECTOR
+__interrupt void timer_A0(void)
+{
+    P1SEL0 &= 0x0;
+    P1SEL2 &= 0x0;
 }
