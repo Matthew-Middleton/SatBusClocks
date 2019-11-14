c5d6d0110d9898bdfe88e9bdefaff62037f3fb8f
Matthew Middleton <mattmidd6@gmail.com>
2019-11-14 11:06:59 -0500

    Made it more inline with cpp.

--- SatCLKS.h
+++ SatCLKS.h
@@ -1,8 +1,21 @@
  #pragma once
  #include <msp430.h>
  
+ namespace SatLib
+ {
+	class CLKS
+	{
+	/*----------------------------------------------------------------------------------------------------------------*/
+	/*CLK registers*/
  
- volatile unsigned int *CSCTL0_H; /*Unlocks/Locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
- volatile unsigned int *CSCTL1;
- volatile unsigned int *CSCTL2;
- volatile unsigned int *CSCTL3;
+	volatile unsigned int *CSCTL0_H; 	/*Unlocks/locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
+	volatile unsigned int *CSCTL1;		/*Sets the frequency for the DCO*/
+	volatile unsigned int *CSCTL2;		/*Sets the CLK to the desired oscillator*/
+	volatile unsigned int *CSCTL3;		/*Sets the CLK divider*/
+	volatile unsigned int *CSCTL4;		/*Sets/adusts the drive strength or turns the specified CLKS off*/
+	volatile unsigned int *CSCTL5;		/*Sets the fault flags for the specified oscillators*/
+	volatile unsigned int *CSCTL6;		/*Sets the given CLKS to be available for conditional module requests*/
+	/*-----------------------------------------------------------------------------------------------------------------*/
+	};
+ }
+ 
