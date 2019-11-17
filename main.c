23e0525cff8befc6843a33389722458078394ec5
Matthew-Middleton <36935493+Matthew-Middleton@users.noreply.github.com>
2019-11-17 17:08:00 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,17 +11,17 @@ void setCLKPreScalar(unsigned int clk, unsigned int Hz, unsigned int divisor)
     //need to determine how to set cycle delay to let the DCO settle based of user input
     /*Delay by ~10us to let DCO settle. 30 cycles = 20 cycles buffer + (10us / (1/1MHz))*/
     __delay_cycles(30);
-	*CSCTL0_H = 0;
+    *CSCTL0_H = 0;
 }
 /**
  * main.c
  */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	/*Stop watchdog timer*/
+    WDTCTL = WDTPW | WDTHOLD;   /*Stop watchdog timer*/
 
-	P1SEL0 |= 0x1;          /*Sets LED1 on the MSP*/
-	P1SEL1 &= 0x0;          /*Clears LED2 on the MSP*/
+    P1DIR |= BIT1;
+    P1OUT |= BIT1;
 
     /*Disable the GPIO power-on default high-impedance mode to activate
     *previously configured port settings
@@ -37,28 +37,27 @@ int main(void)
     __delay_cycles(30);
     CSCTL0_H = 0;           /*Lock CS registers*/
 
-    configTimerA(50000);
-    /*while(1)
+    configTimerA(500);
+    while(1)
     {
-        _bis_SR_register(LPM3_bits+GIE);//what?
-    }*/
-	return 0;
+        _bis_SR_register(GIE);
+    }
+    return 0;
 }
 
 //should configure to check for unsigned overflow
 void configTimerA(unsigned int delayCycles)
 {
 
-    TA0CCTL0 |= CCIE;       /*Enable Interrupts on Timer*/
-    TA0CCR0 = delayCycles;  /*Number of cycles in the timer*/
-    TA0CTL |= TASSEL__SMCLK;/*Use SMCLK as source for timer*/
-    TA0CTL |= MC_1;         /*Use UP mode timer*/
+    TA0CCTL0 |= CCIE;                           /*Enable Interrupts on Timer*/
+    TA0CCR0 = delayCycles;                      /*Number of cycles in the timer*/
+    TA0CTL = TASSEL__SMCLK | MC__UP | ID__1;    /*Use SMCLK as source for timer in UP mode*/
 }
 
 #pragma vector = TIMER0_A0_VECTOR
 __interrupt void timer_A0(void)
 {
-    P1SEL0 &= 0x0;
-    P1SEL2 &= 0x0;
+    P1OUT ^= BIT1;
+    TA0CCTL0 &= ~TAIFG;
 }
 //https://e2e.ti.com/support/microcontrollers/msp430/f/166/t/618978?MSP430FR5994-LPM3-using-timer-interrupt-
