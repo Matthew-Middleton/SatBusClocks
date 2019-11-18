945b1514dd080ab15b07a3cd961eb06d280132e8
Matthew-Middleton <36935493+Matthew-Middleton@users.noreply.github.com>
2019-11-18 07:53:28 -0500

    Update SatCLKS.h

--- SatCLKS.h
+++ SatCLKS.h
@@ -1,21 +1,35 @@
- #pragma once
- #include <msp430.h>
- 
- namespace SatLib
- {
-	typedef struct _CLKREGISTERS
-	{
-	/*----------------------------------------------------------------------------------------------------------------*/
-	/*CLK registers*/
- 
-		volatile unsigned int *CSCTL0_H; 	/*Unlocks/locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
-		volatile unsigned int *CSCTL1;		/*Sets the frequency for the DCO*/
-		volatile unsigned int *CSCTL2;		/*Sets the CLK to the desired oscillator*/
-		volatile unsigned int *CSCTL3;		/*Sets the CLK divider*/
-		volatile unsigned int *CSCTL4;		/*Sets/adusts the drive strength or turns the specified CLKS off*/
-		volatile unsigned int *CSCTL5;		/*Sets the fault flags for the specified oscillators*/
-		volatile unsigned int *CSCTL6;		/*Sets the given CLKS to be available for conditional module requests*/
-	/*-----------------------------------------------------------------------------------------------------------------*/
-	} CLKREGISTERS;
- }
- 
+/*
+ * SatCLKS.h
+ *
+ *  Created on: Nov 18, 2019
+ *      Author: Matthew Middleton
+ */
+#pragma once
+#include <msp430.h>
+
+namespace SatLib
+{
+    class SatCLKS
+    {
+        typedef struct _CLKREGISTERS
+        {
+            /*----------------------------------------------------------------------------------------------------------------*/
+            /*CLK registers*/
+
+            volatile unsigned int *CSCTL0_H;    /*Unlocks/locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
+            volatile unsigned int *CSCTL1;      /*Sets the frequency for the DCO*/
+            volatile unsigned int *CSCTL2;      /*Sets the CLK to the desired oscillator*/
+            volatile unsigned int *CSCTL3;      /*Sets the CLK divider*/
+            volatile unsigned int *CSCTL4;      /*Sets/adusts the drive strength or turns the specified CLKS off*/
+            volatile unsigned int *CSCTL5;      /*Sets the fault flags for the specified oscillators*/
+            volatile unsigned int *CSCTL6;      /*Sets the given CLKS to be available for conditional module requests*/
+            /*-----------------------------------------------------------------------------------------------------------------*/
+        } CLKREGISTERS;
+
+        /*Sets a clock
+         *
+         */
+        void setCLK(unsigned int clk, unsigned int freq, unsigned int div);
+        void configTimerA(unsigned int delayCycles);
+    };
+}
