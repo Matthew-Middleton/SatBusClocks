57fb1dc95b6233c707c2deb11df496132f892b7f
Matthew-Middleton <36935493+Matthew-Middleton@users.noreply.github.com>
2019-11-19 21:20:59 -0500

    Update SatCLKS.h

--- SatCLKS.h
+++ SatCLKS.h
@@ -5,34 +5,31 @@
  *      Author: Matthew Middleton
  */
 #pragma once
+#include <stdint.h>
 #include <msp430.h>
 
-namespace SatLib
+class SatCLKS
 {
-    class SatCLKS
-    {
-        typedef struct _CLKREGISTERS
-        {
-            /*----------------------------------------------------------------------------------------------------------------*/
-            /*CLK registers*/
+    /*----------------------------------------------------------------------------------------------------------------*/
+    /*CLK registers*/
 
-            volatile unsigned int *CSCTL0_H;    /*Unlocks/locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
-            volatile unsigned int *CSCTL1;      /*Sets the frequency for the DCO*/
-            volatile unsigned int *CSCTL2;      /*Sets the CLK to the desired oscillator*/
-            volatile unsigned int *CSCTL3;      /*Sets the CLK divider*/
-            volatile unsigned int *CSCTL4;      /*Sets/adusts the drive strength or turns the specified CLKS off*/
-            volatile unsigned int *CSCTL5;      /*Sets the fault flags for the specified oscillators*/
-            volatile unsigned int *CSCTL6;      /*Sets the given CLKS to be available for conditional module requests*/
-            /*-----------------------------------------------------------------------------------------------------------------*/
-        } CLKREGISTERS;
+    volatile unsigned int *CSCTL0_H;    /*Unlocks/locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
+    volatile unsigned int *CSCTL1;      /*Sets the frequency for the DCO*/
+    volatile unsigned int *CSCTL2;      /*Sets the CLK to the desired oscillator*/
+    volatile unsigned int *CSCTL3;      /*Sets the CLK divider*/
+    volatile unsigned int *CSCTL4;      /*Sets/adusts the drive strength or turns the specified CLKS off*/
+    volatile unsigned int *CSCTL5;      /*Sets the fault flags for the specified oscillators*/
+    volatile unsigned int *CSCTL6;      /*Sets the given CLKS to be available for conditional module requests*/
+    /*-----------------------------------------------------------------------------------------------------------------*/
+    public:
 
+        SatCLKS();
         /*Sets a clock
          *
          */
-        void configClks(unsigned int clk, unsigned int freq, unsigned int div);
+        void configClks(unsigned int clk, unsigned int dco_range, unsigned int freq, unsigned int div);
 
-        void configPrescalarClk();
+        void configPreScalarClk();
 
         void configTimerA(unsigned int delayCycles);
-    };
-}
+};
