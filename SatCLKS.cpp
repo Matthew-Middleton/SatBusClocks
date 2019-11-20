ce589c80ef3a5c2915990e14e528a2dad6c2aba5
Matthew-Middleton <36935493+Matthew-Middleton@users.noreply.github.com>
2019-11-19 19:48:33 -0500

    Update SatCLKS.cpp

--- SatCLKS.cpp
+++ SatCLKS.cpp
@@ -1,25 +1,60 @@
 
 #include "SatCLKS.h"
 
-
-SatLib::SatCLKS::SatCLKS()
+void configClks(unsigned int clk, unsigned int dco_range, unsigned int freq, unsigned int clk_div)
 {
-	
+    uint16_t tempCSCTL3 = 0;
+    tempCSCTL3 = CSCTL3;        //Store CSCTL3 values temporarily
+
+    CLKREGISTERS.(*CSCTL0_H) = CSKEY_H; //Unlocks register
+    CLKREGISTERS.(*CSCTL2) = clk;   //Sets the clock to DCO
+    //Sets the divider according to the clocks provided
+    CLKREGISTERS.(*CSCTL3) = clk_div;
+
+    if(dco_range&DCORSEL)   //The DCO range may exceed 8MHz.
+    {
+
+#ifdef FRCTL0
+        /*Determine how many waitstates are required for the fram
+         * switches based on the bit pattern(see family guide) specified by freq
+         */
+        switch(freq)
+        {
+        //If the frequency is 8 MHz: 8MHz/8MHz = 1
+        case 3:
+            FRCTL0 = FRCTLPW | NWAITS1;
+            break;
+        /*If the frequency is 16 MHz: 16MHz / 8MHz = 2
+         *or the frequency is 21 MHz: 21MHz / 8MHz = 2.625
+         *2 wait states are needed
+         */
+        case 4:
+        case 5:
+            FRCTL0 = FRCTLPW | NWAITS2;
+            break;
+        //If the frequency is 24 MHz: 24MHz / 8MHz = 3 wait states are needed
+        case 6:
+            FRCTL0 = FRCTLPW | NWAITS3;
+            break;
+        default:
+            break;
+        }
+#endif
+
+    }
+    CLKREGISTERS.(*CSCTL1) = dco_range | freq;  //Sets the DCO to the given frequency
+
+    /*Delay by ~10us to let DCO settle, per device errata. k cycles = 20 cycles buffer + (10us / (1/n MHz))*/
+    //is this needed or do the wait states handle the delay?
+    //__delay_cycles(30);//per specs
+
+    CLKREGISTERS.(*CSCTL3) = tempCSCTL3;    //Restore clock settings. Is this necessary?
+    *CSCTL0_H = 0;                  //lock the CS registers
 }
 
-void setCLK(unsigned int clk, unsigned int freq, unsigned int clk_div)
+void configPreScalarClk()
 {
-    CLKREGISTERS.(*CSCTL0_H) = CSKEY_H; //Unlocks register
-#ifdef FRCTL0
-    FRCTL0 = FRCTLPW | NWAITS1;
-#endif
-    CLKREGISTERS.(*CSCTL1) = freq;  //Sets the DCO to the given frequency
-    CLKREGISTERS.(*CSCTL2) = clk;   //Sets the clock to DCO
-    CLKREGISTERS.(*CSCTL3) = clk_div;   //Sets the source divider according to the clocks provided
 
-    /*Delay by ~10us to let DCO settle. 30 cycles = 20 cycles buffer + (10us / (1/1MHz))*/
-    __delay_cycles(4);//per specs
-    *CSCTL0_H = 0;                      //lock the CS registers
 }
 
 void configTimerA(unsigned int delayCycles)
@@ -38,4 +73,5 @@ __interrupt void timer_A0(void)
 }
 //https://e2e.ti.com/support/microcontrollers/msp430/f/166/t/618978?MSP430FR5994-LPM3-using-timer-interrupt-
 //http://e2e.ti.com/support/microcontrollers/msp430/f/166/t/609971?MSP430FR5994-Can-t-get-system-to-work-on-16-MHz
+
 
