c21fb8b6ea847f5d3b56380a669fbbaf0c7a14dd
Matthew Middleton <mattmidd6@gmail.com>
2019-12-06 14:03:11 -0500

    Changed main

--- main.cpp
+++ main.cpp
@@ -5,24 +5,31 @@
  *      Author: Matthew Middleton
  */
 
-#include "SatCLKS.h"
+#include "./SatBusClocks/SatCLKS.h"
+//#include <msp430.h>
 
 int main()
 {
     WDTCTL = WDTPW | WDTHOLD;
 
     PM5CTL0 &= ~LOCKLPM5;
-
+    volatile unsigned int val = 0;
     SatCLKS clock_contr;
-    //P1DIR = BIT1;
-    //P1OUT |= BIT1;
+    P1DIR = 0;
+    P1OUT &= 0x0000;
     P5DIR = BIT7;
     P5SEL1 = BIT1|BIT2;
-    clock_contr.configClks(SELM__DCOCLK, 0, DCOFSEL_0, DIVM__1);
+    /*CSCTL0_H = CSKEY_H;
+    CSCTL2 = SELM__DCOCLK;
+    CSCTL3 = DIVM__1;
+    CSCTL1 = DCOFSEL_1;*/
+    clock_contr.configClks(SELM__DCOCLK, 0, DCOFSEL_2, DIVM__1);
 
     //clock_contr.configTimerA(50320);
     //_bis_SR_register(GIE);
     P5SEL1 = 0x00;
+    //P1DIR = 0x0000;
+    //P1OUT = 0x0000;
     return 0;
 }
 
