f2dd349895eb56da7316437126d980f9c690747f
Matthew Middleton <mattmidd6@gmail.com>
2019-12-06 14:03:50 -0500

    Redefined class variables to avoid variable conflicts

--- SatCLKS.h
+++ SatCLKS.h
@@ -15,13 +15,13 @@ class SatCLKS
     /*----------------------------------------------------------------------------------------------------------------*/
     /*CLK registers*/
 
-    volatile unsigned int *CSCTL0_H;    /*Unlocks/locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
-    volatile unsigned int *CSCTL1;      /*Sets the frequency for the DCO*/
-    volatile unsigned int *CSCTL2;      /*Sets the CLK to the desired oscillator*/
-    volatile unsigned int *CSCTL3;      /*Sets the CLK divider*/
-    volatile unsigned int *CSCTL4;      /*Sets/adusts the drive strength or turns the specified CLKS off*/
-    volatile unsigned int *CSCTL5;      /*Sets the fault flags for the specified oscillators*/
-    volatile unsigned int *CSCTL6;      /*Sets the given CLKS to be available for conditional module requests*/
+    volatile unsigned int *CSCTL0_H_;    /*Unlocks/locks the CS registers for manipulation. CSKEY_H to unlock, 0 to lock*/
+    volatile unsigned int *CSCTL1_;      /*Sets the frequency for the DCO*/
+    volatile unsigned int *CSCTL2_;      /*Sets the CLK to the desired oscillator*/
+    volatile unsigned int *CSCTL3_;      /*Sets the CLK divider*/
+    volatile unsigned int *CSCTL4_;      /*Sets/adusts the drive strength or turns the specified CLKS off*/
+    volatile unsigned int *CSCTL5_;      /*Sets the fault flags for the specified oscillators*/
+    volatile unsigned int *CSCTL6_;      /*Sets the given CLKS to be available for conditional module requests*/
     /*-----------------------------------------------------------------------------------------------------------------*/
     private:
         unsigned int lookup_frequency(unsigned int dco_range, unsigned int freq);
