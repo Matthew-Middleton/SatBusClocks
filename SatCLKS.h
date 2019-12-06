ef519f24d6e927feb2702afd6fca031f8abfc046
Matthew Middleton <mattmidd6@gmail.com>
2019-12-05 21:45:15 -0500

    Added prototype for added functions

--- SatCLKS.h
+++ SatCLKS.h
@@ -2,9 +2,11 @@
  * SatCLKS.h
  *
  *  Created on: Nov 18, 2019
+ *  Updated on: Dec 5, 2019
  *      Author: Matthew Middleton
  */
 #pragma once
+#include <stdlib.h>
 #include <stdint.h>
 #include <msp430.h>
 
@@ -21,15 +23,27 @@ class SatCLKS
     volatile unsigned int *CSCTL5;      /*Sets the fault flags for the specified oscillators*/
     volatile unsigned int *CSCTL6;      /*Sets the given CLKS to be available for conditional module requests*/
     /*-----------------------------------------------------------------------------------------------------------------*/
+    private:
+        unsigned int lookup_frequency(unsigned int dco_range, unsigned int freq);
+
     public:
 
         SatCLKS();
-        /*Sets a clock
-         *
+        /*Configures a clock to the desired clock, clk, the dco range to use, dco_range,
+         * the frequency to set the specified clock to, freq, and the amount to divide the specified clock, div.
+         * Configuring more than one clock at once for different frequencies may caused undesired/unintended
+         * effects, so configure one clock at a time.
+         * Currently there are no checks for invalid conditions besides setting wait states for FRAM.
          */
         void configClks(unsigned int clk, unsigned int dco_range, unsigned int freq, unsigned int div);
 
+        /*Configures a clock for prescaler use with a peripheral
+         *
+         * Incomplete at the moment
+         */
         void configPreScalarClk();
 
+        /*Configures a timer, with interrupts, given the amount to delay
+         */
         void configTimerA(unsigned int delayCycles);
 };
