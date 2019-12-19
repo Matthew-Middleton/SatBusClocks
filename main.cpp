8fc64a17500b93c035737a7425a5b308f335759e
Matthew Middleton <mattmidd6@gmail.com>
2019-12-19 15:02:07 -0500

    modified main

--- main.cpp
+++ main.cpp
@@ -10,27 +10,23 @@
 
 int main()
 {
-    WDTCTL = WDTPW | WDTHOLD;
 
-    PM5CTL0 &= ~LOCKLPM5;
-    volatile unsigned int val = 0;
+    WDTCTL = WDTPW | WDTHOLD;
     SatCLKS clock_contr;
-    P1DIR = 0;
-    P1OUT &= 0x0000;
+    clock_contr.configClks(SELS__DCOCLK | SELM__DCOCLK, 0, DCOFSEL_0, DIVS__1 | DIVM__1);
+
+    P1DIR = BIT1 | BIT0;
+    P1OUT = BIT1;
     P5DIR = BIT7;
     P5SEL1 = BIT1|BIT2;
-    /*CSCTL0_H = CSKEY_H;
-    CSCTL2 = SELM__DCOCLK;
-    CSCTL3 = DIVM__1;
-    CSCTL1 = DCOFSEL_1;*/
-    clock_contr.configClks(SELM__DCOCLK, 0, DCOFSEL_2, DIVM__1);
+    clock_contr.configTimerA(60500);
 
-    //clock_contr.configTimerA(50320);
-    //_bis_SR_register(GIE);
+    PM5CTL0 &= ~LOCKLPM5;
+    _bis_SR_register(GIE);
+
     P5SEL1 = 0x00;
-    //P1DIR = 0x0000;
-    //P1OUT = 0x0000;
+    P1OUT &= ~(BIT1 | BIT0);
+    P1DIR = 0x0000;
+
     return 0;
 }
-
-
